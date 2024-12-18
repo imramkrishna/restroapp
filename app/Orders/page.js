f795b84c0ace10ab9bb689f848c92a70c4b946cf
Ram Krishna Yadav <itsramky234@gmail.com>
2024-12-18 14:38:00 +0545

    Order portion updated

--- app/Orders/page.js
+++ app/Orders/page.js
@@ -2,8 +2,75 @@ import React from 'react'
 
 function page() {
   return (
-    <div>
-      I am Orders
+    <div className='mx-5 min-h-lvh flex flex-wrap'>
+      <div className="order-card mx-3 h-80 w-1/4 border-2 border-e-slate-100 rounded-xl">
+      <h1 className='m-4'>Dine out / Delivery</h1>
+      <div className="order-details mx-4 bg-slate-200 h-36 w-11/12 border-2 border-e-slate-100 rounded-xl">
+      <ul className='flex mx-4'>
+        <li className='w-1/2'>Token No.10</li>
+        <li className='w-1/2'>Status:Paid</li>
+      </ul>
+      <ul className='Orders my-8 mx-4'>
+        <li>French Fries</li>
+        <li>Biryani</li>
+      </ul>
+
+      </div>
+
+
+      </div>
+      <div className="order-card mx-3  h-80 w-1/4 border-2 border-e-slate-100 rounded-xl">
+      <h1 className='m-4'>Dine out / Delivery</h1>
+      <div className="order-details mx-4 bg-slate-200 h-36 w-11/12 border-2 border-e-slate-100 rounded-xl">
+      <ul className='flex mx-4'>
+        <li className='w-1/2'>Token No.10</li>
+        <li className='w-1/2'>Status:Paid</li>
+      </ul>
+      <ul className='Orders my-8 mx-4'>
+        <li>French Fries</li>
+        <li>Biryani</li>
+      </ul>
+
+      </div>
+
+
+      </div>
+      
+      <div className="order-card mx-3 h-80 w-1/4 border-2 border-e-slate-100 rounded-xl">
+      <h1 className='m-4'>Dine out / Delivery</h1>
+      <div className="order-details mx-4 bg-slate-200 h-36 w-11/12 border-2 border-e-slate-100 rounded-xl">
+      <ul className='flex mx-4'>
+        <li className='w-1/2'>Token No.10</li>
+        <li className='w-1/2'>Status:Paid</li>
+      </ul>
+      <ul className='Orders my-8 mx-4'>
+        <li>French Fries</li>
+        <li>Biryani</li>
+      </ul>
+
+      </div>
+
+
+      </div>
+      
+      <div className="order-card mx-3 h-80 w-1/4 border-2 border-e-slate-100 rounded-xl">
+      <h1 className='m-4'>Dine out / Delivery</h1>
+      <div className="order-details mx-4 bg-slate-200 h-36 w-11/12 border-2 border-e-slate-100 rounded-xl">
+      <ul className='flex mx-4'>
+        <li className='w-1/2'>Token No.10</li>
+        <li className='w-1/2'>Status:Paid</li>
+      </ul>
+      <ul className='Orders my-8 mx-4'>
+        <li>French Fries</li>
+        <li>Biryani</li>
+      </ul>
+
+      </div>
+
+
+      </div>
+      
+      
     </div>
   )
 }
